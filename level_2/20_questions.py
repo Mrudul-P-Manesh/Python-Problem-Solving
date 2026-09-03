527ae53974db68c6595f5cbde11179b4d07826f5
Mrudul-P-Manesh <mrudulpmanesh2007@gmail.com>
2026-09-03 09:08:47 +0530

    20

---
+++ level_2/20_questions.py
@@ -0,0 +1,10 @@
+count = 0
+for num in range(2, 10):
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        count += 1
+print(count)
